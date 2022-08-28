d5aeb6f9a67f7b36feffb60bdacd09413969d533
Your Name <applezafar@gmail.com>
2022-08-29 00:12:28 +1000

    first commit активити 2

---
+++ app/src/main/java/com/example/applicationactivities88/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.example.applicationactivities88
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity(R.layout.activity_second){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
